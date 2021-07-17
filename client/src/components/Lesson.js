ea5657650638c8a4cfa9bd06e6cb108d32261244
Shen Rui <chongshenrui98@gmail.com>
2021-07-17 17:43:03 +0800

    Initial fix for rendering of overlapping lessons

--- client/src/components/Lesson.js
+++ client/src/components/Lesson.js
@@ -7,7 +7,9 @@ function Lesson(props) {
         //     CZ3002 TUT TS8 SWLAB3 Wk2,4,6,8,10,12
         // </div>     
         //  style = {{marginLeft: props.marginLeft, width: props.width }}
-        <div className=" border border-dark lessonText timetableRow" style={{backgroundColor: props.color, width: props.width, transform: `translateX(${props.offset})`}}>
+        <div className=" border border-dark lessonText timetableRow" 
+             style={{backgroundColor: props.color, width: props.width, height: props.height,
+                     transform: `translate(${props.offset}, ${props.verticalOffset})` }}>
             {`${props.courseCode} ${props.type} ${props.group} ${props.venue} ${props.teachingWeeks}`}
         </div>  
         // <div className="lessonText timetableRow" style={{width: "6.4vw", transform: `translateX(5vw)`}}>
