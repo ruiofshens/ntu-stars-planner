7d69b258a02c0cb22d01065c56c46ee7b5536f57
Jun Wei <tohjunwei.tj@gmail.com>
2022-12-04 12:36:37 +0800

    change api endpoint

--- client/src/services/DataRetriever.js
+++ client/src/services/DataRetriever.js
@@ -1,53 +1,56 @@
 import axios from 'axios';
 
-const url = "https://starspanel-api.herokuapp.com/courses/";
+const url = 'https://stars-panel-be.fly.dev/courses/';
 
 export const fetchAllCourses = async () => {
-     
-    try {
-        const { data } = await axios.get(url)
-        return data;
-    }
-    catch (error) {
-        console.log(error.message);
-    }
-    
-}
+  try {
+    const { data } = await axios.get(url);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const fetchCourses = async (courseCodes) => {
-    //courseCodes: array
-    try {
-        const { data } = await axios.get(url + "courses?courseCodes=" + courseCodes.join(","));
-        return data;
-    } catch (error) {
-        console.log(error.message);
-    }
-} 
+  //courseCodes: array
+  try {
+    const { data } = await axios.get(
+      url + 'courses?courseCodes=' + courseCodes.join(',')
+    );
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const fetchVacanciesAndWaitlist = async (courseCode) => {
-    try {
-        const { data } = await axios.get(url + "vacancies?courseCode=" + courseCode);
-        return data;
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const { data } = await axios.get(
+      url + 'vacancies?courseCode=' + courseCode
+    );
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const fetchExams = async (courseCodes) => {
-    //courseCodes: array
-    try {
-        const { data } = await axios.get(url + "exams?courseCodes=" + courseCodes.join(",")); 
-        return data;
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  //courseCodes: array
+  try {
+    const { data } = await axios.get(
+      url + 'exams?courseCodes=' + courseCodes.join(',')
+    );
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 export const fetchAcadSem = async () => {
-    try {
-        const { data } = await axios.get(url + "acadSem");
-        return data;
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  try {
+    const { data } = await axios.get(url + 'acadSem');
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
